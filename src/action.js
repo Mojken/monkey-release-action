b62b3310c5eb1f022af220e133c5d358fb5651a2
Mojken <mikaelgillis@gmail.com>
2021-11-05 21:30:03 +0100

    handle edge-case of no previous release (#4)

--- src/action.js
+++ src/action.js
@@ -282,20 +282,23 @@ async function setStatus(pullRequest, state, description) {
 
 async function generateReleaseNotes(pullRequest) {
   const tag = getTagName(pullRequest);
-  core.info("generating releasenotes");
 
+  var previous_tag_name;
   try {
     const latest_release = await client.rest.repos.getLatestRelease({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
     });
-
+    previous_tag_name = latest_release.data.tag_name;
     core.info(
-      "Generating release-notes relative to release " +
-        latest_release.data.tag_name +
-        ".."
+      "Generating release-notes relative to release " + previous_tag_name + ".."
     );
+  } catch (error) {
+    previous_tag_name = "";
+    core.info("Generating release-notes relative to start of repository..");
+  }
 
+  try {
     const response = await client.request(
       "POST /repos/{owner}/{repo}/releases/generate-notes",
       {
@@ -303,7 +306,7 @@ async function generateReleaseNotes(pullRequest) {
         repo: github.context.repo.repo,
         tag_name: tag,
         target_commitish: pullRequest.head.ref,
-        previous_tag_name: latest_release.data.tag_name,
+        previous_tag_name: previous_tag_name,
       }
     );
 
